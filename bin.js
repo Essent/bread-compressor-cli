2cb9a9fcef1e87e9fc4b03bdab42ccfa289b27c1
Henk Bakker <spike1292@gmail.com>
2020-10-06 13:37:58 +0200

    run commander once

--- bin.js
+++ bin.js
@@ -1,7 +1,45 @@
 #!/usr/bin/env node
 
 const compressor = require("./index");
+const program = require("commander");
+
+program
+  .version("1.0.6")
+  .usage("[options] <globs ...>")
+  .option("-s, --stats", "Show statistics")
+  .option(
+    "-a, --algorithm <items>",
+    'Comma separated list of compression algorithms. Supported values are "brotli" and "gzip". Default "brotli,gzip"',
+    (items) => items.split(",")
+  )
+  .option("-n, --no-default-ignores", "Do not add default glob ignores")
+  .option(
+    "-l, --limit <value>",
+    "Number of tasks running concurrently. Default is your total number of cores",
+    parseInt
+  )
+  .option(
+    "--zopfli-numiterations <value>",
+    "Maximum amount of times to rerun forward and backward pass to optimize LZ77 compression cost. Good values: 10, 15 for small files, 5 for files over several MB in size or it will be too slow. Default 15",
+    parseInt
+  )
+  .option(
+    "--zopfli-blocksplittinglast <value>",
+    'If "true", chooses the optimal block split points only after doing the iterative LZ77 compression. If "false", chooses the block split points first, then does iterative LZ77 on each individual block. If "both", first runs with false, then with true and keeps the smaller file. Default "false"'
+  )
+  .option(
+    "--brotli-mode <value>",
+    "0 = generic, 1 = text (default), 2 = font (WOFF2)",
+    parseInt
+  )
+  .option("--brotli-quality <value>", "0 - 11. Default 11", parseInt)
+  .option("--brotli-lgwin <value>", "Window size. Default 22", parseInt)
+  .parse(process.argv);
+
 compressor
-  .compress("gzip")
-  .then(() => compressor.compress("brotli"))
-  .catch(console.log);
+  .compress(program, "gzip")
+  .then(() => compressor.compress(program, "brotli"))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- index.js
+++ index.js
@@ -6,48 +6,15 @@ const fork = require("child_process").fork;
 const os = require("os");
 const chalk = require("chalk");
 
-const program = require("commander");
-
 module.exports = {
   compress,
 };
 
-function parseArgs(algorithm) {
-  program
-    .version("1.0.6")
-    .usage("[options] <globs ...>")
-    .option("-s, --stats", "Show statistics")
-    .option(
-      "-a, --algorithm <items>",
-      'Comma separated list of compression algorithms. Supported values are "brotli" and "gzip". Default "brotli,gzip"',
-      (items) => items.split(",")
-    )
-    .option("-n, --no-default-ignores", "Do not add default glob ignores")
-    .option(
-      "-l, --limit <value>",
-      "Number of tasks running concurrently. Default is your total number of cores",
-      parseInt
-    )
-    .option(
-      "--zopfli-numiterations <value>",
-      "Maximum amount of times to rerun forward and backward pass to optimize LZ77 compression cost. Good values: 10, 15 for small files, 5 for files over several MB in size or it will be too slow. Default 15",
-      parseInt
-    )
-    .option(
-      "--zopfli-blocksplittinglast <value>",
-      'If "true", chooses the optimal block split points only after doing the iterative LZ77 compression. If "false", chooses the block split points first, then does iterative LZ77 on each individual block. If "both", first runs with false, then with true and keeps the smaller file. Default "false"'
-    )
-    .option(
-      "--brotli-mode <value>",
-      "0 = generic, 1 = text (default), 2 = font (WOFF2)",
-      parseInt
-    )
-    .option("--brotli-quality <value>", "0 - 11. Default 11", parseInt)
-    .option("--brotli-lgwin <value>", "Window size. Default 22", parseInt)
-    .parse(process.argv);
-}
-
-function addDefaultIgnores() {
+/**
+ *
+ * @param {import('commander')} program
+ */
+function addDefaultIgnores(program) {
   if (program.defaultIgnores) {
     const globs = program.args.slice();
     for (const ignore of [
@@ -68,8 +35,12 @@ function addDefaultIgnores() {
   return program.args;
 }
 
-async function compress(algorithm) {
-  parseArgs(algorithm);
+/**
+ *
+ * @param {import('commander')} program
+ * @param {'brotli'|'gzip'} algorithm
+ */
+async function compress(program, algorithm) {
   if (!program.args || program.args.length === 0) {
     program.help();
   }
